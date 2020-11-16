49a479984be7e08acfae25df05164f0019f2d629
haonanjia <nannan06200@gmail.com>
2020-11-17 01:41:47 +0800

    Add files via upload

---
+++ 作业/Employee3.java
@@ -0,0 +1,40 @@
+package haonanjia;
+public class Employee3 extends StaffMember3
+{
+    public final int STANDARD_VACATION = 14;
+
+    protected String socialSecurityNumber;
+    protected double payRate;
+
+   
+    public Employee3(String eName, String eAddress, String ePhone,
+            String socSecNumber, double rate)
+    {
+        super(eName, eAddress, ePhone);
+
+        socialSecurityNumber = socSecNumber;
+        payRate = rate;
+    }
+
+
+    public String toString()
+    {
+        String result = super.toString();
+
+        result += "\nSocial Security Number: " + socialSecurityNumber;
+
+        return result;
+    }
+
+   
+    public double pay()
+    {
+        return payRate;
+    }
+
+ 
+    public int vacation()
+    {
+        return STANDARD_VACATION;
+    }
+}
